7b00aab0aa56af4d080eeb415bf7bb87e31d9051
skaran921 <skaran921@gmail.com>
2020-08-24 15:51:30 +0530

    #visitorTypeScreenDone

--- ivisiteur/src/routes.js
+++ ivisiteur/src/routes.js
@@ -2,12 +2,16 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "./app";
 import PageNotFound from "./components/error/PageNotFound";
+import VisitorType from "./views/visitors/VisitorTypes";
+import Step1 from "./views/visitors/Step1";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={App} />
+        <Route path="/visitorType" component={VisitorType} />
+        <Route path="/step1/:visitorTypeId" component={Step1} />
         <Route path="*" component={PageNotFound} />
       </Switch>
     </BrowserRouter>
